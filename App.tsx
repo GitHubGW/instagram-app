924c64831ed07e443634ac2f50664b50702e482e
GW <kowonp@gmail.com>
2022-03-30 21:15:43 +0900

    feat(App.tsx): set dark mode

--- App.tsx
+++ App.tsx
@@ -1,28 +1,28 @@
+import "react-native-gesture-handler";
+import { useEffect, useState } from "react";
 import AppLoading from "expo-app-loading";
-import { useState } from "react";
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import * as Font from "expo-font";
-import { Asset, useAssets } from "expo-asset";
-
-interface IoniconsFont {
-  [key: string]: any;
-}
+import { Asset } from "expo-asset";
+import LoggedOutNav from "./navigators/LoggedOutNav";
+import { NavigationContainer } from "@react-navigation/native";
+import { Appearance } from "react-native";
+import { darkTheme, lightTheme } from "./styles/themes";
+import { ThemeProvider } from "styled-components/native";
 
 const App = () => {
+  const [currentTheme, setCurrentTheme] = useState<"light" | "dark">("light");
   const [loading, setLoading] = useState<boolean>(true);
 
   const startAsync = async (): Promise<void> => {
-    const ioniconsFontArray: IoniconsFont[] = [Ionicons.font];
-    const loadedIoniconsFont: Promise<void>[] = ioniconsFontArray.map((ioniconsFont: IoniconsFont) => Font.loadAsync(ioniconsFont));
+    const ioniconsFontArray: { [key: string]: any }[] = [Ionicons.font];
+    const loadedIoniconsFont: Promise<void>[] = ioniconsFontArray.map((ioniconsFont: { [key: string]: any }) => Font.loadAsync(ioniconsFont));
     const imageArray: string[] = [
       require("./assets/instagram_logo.png"),
       "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/840px-Instagram_logo.svg.png",
     ];
-    const loadedImage: Promise<Asset[]>[] = imageArray.map((image) => Asset.loadAsync(image));
-    // Promise.all<Promise<void> | Promise<Asset[]>>([...loadedIoniconsFont, ...loadedImage]);
-    Promise.all([loadedIoniconsFont, loadedImage]);
+    const loadedImage: Promise<Asset[]>[] = imageArray.map((image: string) => Asset.loadAsync(image));
+    Promise.all([loadedIoniconsFont, loadedImage]); // Promise.all<Promise<void> | Promise<Asset[]>>([...loadedIoniconsFont, ...loadedImage]);
   };
 
   const onFinish = (): void => {
@@ -34,27 +34,29 @@ const App = () => {
     console.log("onError");
   };
 
+  useEffect(() => {
+    Appearance.addChangeListener(({ colorScheme }) => {
+      if (colorScheme === "dark") {
+        setCurrentTheme("dark");
+      } else {
+        setCurrentTheme("light");
+      }
+    });
+  }, [currentTheme]);
+
   return (
     <>
       {loading === true ? (
         <AppLoading startAsync={startAsync} onFinish={onFinish} onError={onError} />
       ) : (
-        <View style={styles.container}>
-          <Text>hello</Text>
-          <StatusBar style="auto" />
-        </View>
+        <ThemeProvider theme={currentTheme === "light" ? lightTheme : darkTheme}>
+          <NavigationContainer>
+            <LoggedOutNav></LoggedOutNav>
+          </NavigationContainer>
+        </ThemeProvider>
       )}
     </>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
 
 export default App;
