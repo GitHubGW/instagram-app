0124f63eb85f478e16f02a65613e5b6208602507
GW <kowonp@gmail.com>
2022-04-05 15:06:51 +0900

    feat(SignUp.tsx): add sign up screen

---
+++ screens/SignUp.tsx
@@ -0,0 +1,136 @@
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { MutableRefObject, RefObject, useRef } from "react";
+import styled from "styled-components/native";
+import AuthLayout from "../components/AuthLayout";
+import Button from "../components/Button";
+import { Input } from "../components/shared";
+import { RootStackParamList } from "../shared/shared.types";
+import { useForm, Controller } from "react-hook-form";
+import { CreateAccountMutation, useCreateAccountMutation } from "../generated/graphql";
+
+type SignUpProps = NativeStackScreenProps<RootStackParamList, "SignUp">;
+
+interface SignUpFormData {
+  email: string;
+  name: string;
+  username: string;
+  password: string;
+}
+
+const Contaienr = styled.View`
+  width: 100%;
+  padding: 0 20px;
+`;
+
+const SignUp = ({ navigation }: SignUpProps) => {
+  const nameRef: MutableRefObject<null> = useRef(null);
+  const usernameRef: MutableRefObject<null> = useRef(null);
+  const passwordRef: MutableRefObject<null> = useRef(null);
+  const { control, handleSubmit, getValues } = useForm<SignUpFormData>({ defaultValues: { email: "", name: "", username: "", password: "" } });
+  const [createAccountMutation, { loading: createAccountLoading }] = useCreateAccountMutation({
+    onCompleted: (data: CreateAccountMutation) => {
+      if (data.createAccount.ok === false) {
+        return;
+      }
+      const { username, password } = getValues();
+      navigation.navigate("Login", { username, password });
+    },
+  });
+
+  const onFocusNext = (nextRef: RefObject<HTMLInputElement>): void => {
+    nextRef.current?.focus();
+  };
+
+  const onValid = (): void => {
+    if (createAccountLoading === true) {
+      return;
+    }
+    const { email, name, username, password } = getValues();
+    createAccountMutation({ variables: { email, name, username, password } });
+  };
+
+  return (
+    <AuthLayout>
+      <Contaienr>
+        <Controller
+          name="email"
+          control={control}
+          rules={{ required: true, minLength: 1, maxLength: 20 }}
+          render={({ field: { onChange, value } }) => (
+            <Input
+              value={value}
+              onChangeText={onChange}
+              onSubmitEditing={() => onFocusNext(nameRef)}
+              keyboardType="email-address"
+              autoCapitalize="none"
+              autoCorrect={false}
+              returnKeyType="done"
+              placeholder="이메일"
+              placeholderTextColor="gray"
+              blurOnSubmit={false}
+            ></Input>
+          )}
+        ></Controller>
+        <Controller
+          name="name"
+          control={control}
+          rules={{ required: true, minLength: 1, maxLength: 20 }}
+          render={({ field: { onChange, value } }) => (
+            <Input
+              value={value}
+              onChangeText={onChange}
+              onSubmitEditing={() => onFocusNext(usernameRef)}
+              ref={nameRef}
+              keyboardType="email-address"
+              autoCapitalize="none"
+              autoCorrect={false}
+              returnKeyType="done"
+              placeholder="이름"
+              placeholderTextColor="gray"
+              blurOnSubmit={false}
+            ></Input>
+          )}
+        ></Controller>
+        <Controller
+          name="username"
+          control={control}
+          rules={{ required: true, minLength: 1, maxLength: 20 }}
+          render={({ field: { onChange, value } }) => (
+            <Input
+              value={value}
+              onChangeText={onChange}
+              onSubmitEditing={() => onFocusNext(passwordRef)}
+              keyboardType="email-address"
+              autoCapitalize="none"
+              autoCorrect={false}
+              returnKeyType="done"
+              placeholder="유저명"
+              placeholderTextColor="gray"
+              blurOnSubmit={false}
+            ></Input>
+          )}
+        ></Controller>
+        <Controller
+          name="password"
+          control={control}
+          rules={{ required: true, minLength: 1, maxLength: 30 }}
+          render={({ field: { onChange, value } }) => (
+            <Input
+              value={value}
+              onChangeText={onChange}
+              onSubmitEditing={handleSubmit(onValid)}
+              ref={passwordRef}
+              autoCapitalize="none"
+              secureTextEntry={true}
+              placeholder="비밀번호"
+              placeholderTextColor="gray"
+            ></Input>
+          )}
+        ></Controller>
+        <Button onPress={handleSubmit(onValid)} text="회원가입" size="14px" bgFill={true} loading={false} disabled={false}></Button>
+      </Contaienr>
+    </AuthLayout>
+  );
+};
+
+export default SignUp;
