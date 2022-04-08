b905a315e30da387f1cc6ea011033c03e1229158
GW <kowonp@gmail.com>
2022-04-09 04:08:09 +0900

    feat(useLoggedInUser.tsx): add useLoggedInUser hook

---
+++ hooks/useLoggedInUser.ts
@@ -0,0 +1,21 @@
+import { ApolloClient, useApolloClient, useReactiveVar } from "@apollo/client";
+import { useEffect } from "react";
+import { handleLogout, isLoggedInVar } from "../apollo";
+import { useSeeMeQuery } from "../generated/graphql";
+
+const useLoggedInUser = () => {
+  const client: ApolloClient<object> = useApolloClient();
+  const isLoggedIn: boolean = useReactiveVar(isLoggedInVar);
+  const { data: seeMeData } = useSeeMeQuery({ skip: isLoggedIn === false });
+
+  useEffect(() => {
+    if (seeMeData?.seeMe.user === null) {
+      console.log("유효하지 않은 토큰입니다. 로그아웃 처리되었습니다.");
+      handleLogout();
+    }
+  }, [seeMeData]);
+
+  return seeMeData?.seeMe.user;
+};
+
+export default useLoggedInUser;
