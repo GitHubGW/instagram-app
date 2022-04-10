59f3d1ab78fd6430e657acb51ba5a9fdcb6dd5ed
GW <kowonp@gmail.com>
2022-04-11 01:47:58 +0900

    feat(KeyboardAvoidingLayout.tsx): add KeyboardAvoidingLayout

--- components/AuthLayout.tsx
+++ components/AuthLayout.tsx
@@ -1,20 +1,11 @@
 import { useReactiveVar } from "@apollo/client";
-import { Keyboard, KeyboardAvoidingView, Platform, TouchableWithoutFeedback } from "react-native";
 import styled from "styled-components/native";
 import { isDarkModeVar } from "../apollo";
+import KeyboardAvoidingLayout from "./KeyboardAvoidingLayout";
 
 interface AuthLayoutProps {
   children: React.ReactNode;
 }
-
-const TouchableWithoutFeedbackContainer = styled(TouchableWithoutFeedback)`
-  flex: 1;
-  background-color: ${(props) => props.theme.bgColor};
-`;
-
-const KeyboardAvoidingViewContainer = styled(KeyboardAvoidingView)`
-  width: 100%;
-`;
 
 const Container = styled.View`
   flex: 1;
@@ -37,12 +28,8 @@ const LogoImage = styled.Image`
 const AuthLayout = ({ children }: AuthLayoutProps) => {
   const isDarkMode: "light" | "dark" = useReactiveVar(isDarkModeVar);
 
-  const handleHideKeyboard = (): void => {
-    Keyboard.dismiss();
-  };
-
   return (
-    <TouchableWithoutFeedbackContainer onPress={handleHideKeyboard} disabled={Platform.OS === "web"}>
+    <KeyboardAvoidingLayout>
       <Container>
         <LogoContainer>
           {isDarkMode === "dark" ? (
@@ -51,11 +38,9 @@ const AuthLayout = ({ children }: AuthLayoutProps) => {
             <LogoImage source={require("../assets/instagram_logo_light.png")} resizeMode="contain"></LogoImage>
           )}
         </LogoContainer>
-        <KeyboardAvoidingViewContainer behavior={Platform.OS === "ios" ? "padding" : "height"} keyboardVerticalOffset={Platform.OS === "ios" ? 50 : -50}>
-          {children}
-        </KeyboardAvoidingViewContainer>
+        {children}
       </Container>
-    </TouchableWithoutFeedbackContainer>
+    </KeyboardAvoidingLayout>
   );
 };
 
---
+++ components/KeyboardAvoidingLayout.tsx
@@ -0,0 +1,31 @@
+import { Keyboard, KeyboardAvoidingView, Platform, TouchableWithoutFeedback } from "react-native";
+import styled from "styled-components/native";
+
+interface KeyboardAvoidingLayoutProps {
+  children: React.ReactNode;
+}
+
+const TouchableWithoutFeedbackContainer = styled(TouchableWithoutFeedback)`
+  flex: 1;
+  background-color: ${(props) => props.theme.bgColor};
+`;
+
+const KeyboardAvoidingViewContainer = styled(KeyboardAvoidingView)`
+  flex: 1;
+`;
+
+const KeyboardAvoidingLayout = ({ children }: KeyboardAvoidingLayoutProps) => {
+  const handleHideKeyboard = (): void => {
+    Keyboard.dismiss();
+  };
+
+  return (
+    <TouchableWithoutFeedbackContainer onPress={handleHideKeyboard} disabled={Platform.OS === "web"}>
+      <KeyboardAvoidingViewContainer behavior={Platform.OS === "ios" ? "padding" : "height"} keyboardVerticalOffset={Platform.OS === "ios" ? 50 : -50}>
+        {children}
+      </KeyboardAvoidingViewContainer>
+    </TouchableWithoutFeedbackContainer>
+  );
+};
+
+export default KeyboardAvoidingLayout;
