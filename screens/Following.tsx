bc8c7d86a6e5a0a46799598037b798e70bd75f09
GW <kowonp@gmail.com>
2022-04-24 17:33:06 +0900

    feat(Following.tsx): add following

---
+++ screens/Following.tsx
@@ -0,0 +1,48 @@
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { useState } from "react";
+import { FlatList } from "react-native";
+import styled from "styled-components/native";
+import UserItem from "../components/UserItem";
+import { useSeeProfileQuery } from "../generated/graphql";
+import { RootStackParamList } from "../shared/shared.types";
+
+type FolloweringProps = NativeStackScreenProps<RootStackParamList, "StackFollowering">;
+
+const Container = styled.View`
+  flex: 1;
+  background-color: ${(props) => props.theme.bgColor};
+`;
+
+const FlatListContainer = styled(FlatList)`
+  width: 100%;
+`;
+
+const Followering = ({ route }: FolloweringProps) => {
+  const [refreshing, setRefreshing] = useState<boolean>(false);
+  const { refetch } = useSeeProfileQuery({ variables: { username: route.params?.username } });
+
+  const onRefresh = async (): Promise<void> => {
+    setRefreshing(true);
+    await refetch();
+    setRefreshing(false);
+  };
+
+  const renderItem = ({ item: following }: any) => {
+    return <UserItem {...following} />;
+  };
+
+  return (
+    <Container>
+      <FlatListContainer
+        showsVerticalScrollIndicator={false}
+        refreshing={refreshing}
+        onRefresh={onRefresh}
+        data={route.params?.following}
+        renderItem={renderItem}
+        keyExtractor={(following: any) => String(following.id)}
+      ></FlatListContainer>
+    </Container>
+  );
+};
+
+export default Followering;
