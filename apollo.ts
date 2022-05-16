60419c57be231d1ce66a1abcf5dbdd8d16bf54ae
GW <kowonp@gmail.com>
2022-05-17 03:46:39 +0900

    feat(apollo.ts): set ApolloClient link

--- apollo.ts
+++ apollo.ts
@@ -1,10 +1,17 @@
-import { ApolloClient, ApolloLink, createHttpLink, InMemoryCache, makeVar, NormalizedCacheObject } from "@apollo/client";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { createClient } from "graphql-ws";
+import { onError } from "@apollo/client/link/error";
+import { WebSocketLink } from "@apollo/client/link/ws";
+import { createUploadLink } from "apollo-upload-client";
 import { setContext } from "@apollo/client/link/context";
+import { GraphQLWsLink } from "@apollo/client/link/subscriptions";
+import { FragmentDefinitionNode, OperationDefinitionNode } from "graphql";
+import { getMainDefinition, offsetLimitPagination } from "@apollo/client/utilities";
+import { ApolloClient, ApolloLink, createHttpLink, InMemoryCache, makeVar, NormalizedCacheObject, split } from "@apollo/client";
 
 export const isLoggedInVar = makeVar<boolean>(false);
 export const tokenVar = makeVar<string>("");
-export const isDarkModeVar = makeVar<boolean>(false);
+export const isDarkModeVar = makeVar<"light" | "dark">("light");
 
 export const handleLogin = async (token: string): Promise<void> => {
   try {
@@ -22,6 +29,7 @@ export const handleLogout = async (): Promise<void> => {
   try {
     const keys: string[] = ["token", "isLoggedIn"];
     await AsyncStorage.multiRemove(keys);
+    client.clearStore();
     isLoggedInVar(false);
     tokenVar("");
   } catch (error) {
@@ -29,15 +37,82 @@ export const handleLogout = async (): Promise<void> => {
   }
 };
 
-const httpLink: ApolloLink = createHttpLink({ uri: "https://0c5c-110-15-165-216.ngrok.io/graphql" });
+const httpLink: ApolloLink = createHttpLink({
+  uri: "http://localhost:4000/graphql",
+});
 
 const authLink: ApolloLink = setContext((_, { headers }) => {
   return { headers: { ...headers, token: tokenVar() } };
 });
 
+const errorLink: ApolloLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors) {
+    console.log("graphQLErrors", graphQLErrors);
+  }
+  if (networkError) {
+    console.log("networkError", networkError);
+  }
+});
+
+const uploadHttpLink: ApolloLink = createUploadLink({
+  uri: "https://instagram-gw.herokuapp.com/graphql",
+});
+
+const uploadHttpLinks: ApolloLink = authLink.concat(errorLink).concat(uploadHttpLink);
+
+/* GraphQLWsLink
+const wsLink: GraphQLWsLink = new GraphQLWsLink(
+  createClient({
+    url: "ws://instagram-gw.herokuapp.com/graphql",
+    connectionParams: () => ({
+      token: tokenVar(),
+    }),
+  })
+);
+*/
+
+const wsLink = new WebSocketLink({
+  uri: "ws://instagram-gw.herokuapp.com/graphql",
+  options: {
+    reconnect: true,
+    connectionParams: () => ({
+      token: tokenVar(),
+    }),
+  },
+});
+
+const splitLink = split(
+  ({ query }) => {
+    const definition: OperationDefinitionNode | FragmentDefinitionNode = getMainDefinition(query);
+    const isSubscription: boolean = definition.kind === "OperationDefinition" && definition.operation === "subscription";
+    return isSubscription;
+  },
+  wsLink,
+  uploadHttpLinks
+);
+
+export const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        seeFeed: {
+          keyArgs: false,
+          merge(existing, incoming) {
+            if (existing) {
+              const result = { ...existing, ...incoming, photos: [...existing.photos, ...incoming.photos] };
+              return result;
+            }
+            return incoming;
+          },
+        },
+      },
+    },
+  },
+});
+
 const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
+  link: splitLink,
+  cache,
 });
 
 export default client;
